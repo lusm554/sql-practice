121540146852c3683a38e598f6e90f0c3401d2a2
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-24 02:56:30 +0300

    popular services

---
+++ src/8-popular-services.sql
@@ -0,0 +1,10 @@
+-- probably i would be better use here rank, but i'm not sure.
+select
+  t.v_name
+from
+  services ss
+inner join tariff_plan t
+  using (id_tariff_plan)
+group by t.v_name
+order by count(id_contract_inst) desc;
+
